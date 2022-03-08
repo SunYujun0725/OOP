e02da8e56725d66223fa83f3e116d5af32ad9830
SunYujun0725 <91317039+SunYujun0725@users.noreply.github.com>
2022-03-09 03:34:25 +0800

    Update Avg.cpp

--- Avg.cpp
+++ Avg.cpp
@@ -7,7 +7,6 @@
 
 #include <iostream>
 #include <cstring>
-#include <string>
 #include <cstdlib>
 
 using namespace std;
@@ -18,46 +17,47 @@ using std::endl;
 
 int main() {
     
-    double number=0;
-    double sum=0;
+    double number=0;  //有幾筆資料
+    double sum=0;     //總和
     long length=0;
-    double avg=0;
-    int checkfloat=0; //確認是否有小數點 //0:沒有
+    double avg=0;     //平均
+    int checkfloat=0; //確認是否有小數點 0:沒有
 
-    string grade;
+    string grade;  //成績（string）
     while( 1 ){    //重複輸入計算多次平均
         checkfloat=0;
         std::cout << "Please input your score :" << endl;
-        std::cin >> grade;
+        std::cin >> grade;  //輸入字串
         if(grade.compare("end")==0){  //輸入end代表結束程式
             break;
         }
         number++;
-        length=grade.length();
-        for(long j=0;j<length;j++){
-            if(grade[j]=='.'){
+        length=grade.length();    //計算字串長度
+        for(long j=0;j<length;j++){   
+            if(grade[j]=='.'){    //有小數點＝>錯誤輸入
                 std::cout << "illegal input, ending the program..." << endl;
-                sum=0;
+                sum=0;            //歸零
                 number=0;
-                checkfloat=1;  //是小數點
+                checkfloat=1;     //是小數點
             }
         }
-        if(checkfloat==0){
-            if(atoi(grade.c_str())>=0&&atoi(grade.c_str())<=100){
-                sum+=atoi(grade.c_str());
-            }
-            else if(grade.compare("-1")==0){
+        if(checkfloat==0){  //沒有小數點
+
+            if(atoi(grade.c_str())>=0&&atoi(grade.c_str())<=100){ //判斷是否在0~100間
+                sum+=atoi(grade.c_str()); //sum加總
+            }  
+
+            else if(grade.compare("-1")==0){   //字串為-1＝>印出平均
                 avg=sum/(number-1);
                 std::cout << "Your average score is :";
                 std::cout << avg << endl;
-            }
-            else if(grade.compare("-1")!=0&&atoi(grade.c_str())==-1){
-                sum=0;
+                sum=0;     //歸零
                 number=0;
             }
-            else {
-                std::cout << "illegal input, ending the program..." << endl;
-                sum=0;
+
+            else {  //其他為錯誤輸入
+                std::cout << "illegal input, ending the program..." << endl;  
+                sum=0;     //歸零
                 number=0;
             }
         }
